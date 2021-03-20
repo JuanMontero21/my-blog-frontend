ec309b492012ff5fe848c3a2a03a95b2e4a6a464
juanysho <juanmonterocallealta@gmail.com>
2021-03-20 12:48:36 +0100

    banner and banner-navigation component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { BannerNavigationComponent } from './shared/banner-navigation/banner-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    BannerNavigationComponent
   ],
   imports: [
     BrowserModule
